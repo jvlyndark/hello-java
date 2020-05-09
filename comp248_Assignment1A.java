785bb7d7a3c82ebf78d020807af8f2b2b9180588
jvlyndark <50807142+jvlyndark@users.noreply.github.com>
2020-05-09 15:42:30 -0400

    Add files via upload

---
+++ comp248_Assignment1A.java
@@ -0,0 +1,28 @@
+// -------------------------------------------------------
+// Assignment Assignment 1A
+// Written by: @jvlyndark
+// For COMP 248 Section EC – Fall 2019
+// --------------------------------------------------------
+
+public class comp248_Assignment1A {
+
+	public static void main(String[] args) {
+		
+		//Printing lines, using escape sequences to properly format
+		System.out.println("Welcome to COMP 248 Java Programming!"
+				+ "\nLet's use programming to have some coffee when you work!"
+				+ "\nwhile (Working)"
+				+ "\n{"
+				+ "\n\tCoffeeMug.Drink();"
+				+ "\n\tworkTask.Execute();"
+				+ "\n\tif (coffeeMug == \"Empty\")"
+				+ "\n\t{"
+				+ "\n\t\tif (coffePot == \"Empty\")"
+				+ "\n\t\t\tcoffeePot.Make();"
+				+ "\n\t\tcoffeMug.Refill();"
+				+ "\n\t}"
+				+ "\n\tEnjoy your coffee!"
+				+ "\n}");
+	}
+
+}
